7da0f233dda5ed47ea4df1ac45bd5fb5826a90c9
Shishkina <pavlova.ya.v@gmail.com>
2016-11-23 09:44:01 +0300

    2.1 2

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
-sudo gunicorn -b 0.0.0.0:8080 hello:application
+#sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+#sudo /etc/init.d/gunicorn restart
+#sudo gunicorn -b 0.0.0.0:8080 hello:application
 
 sudo ln -sf /home/box/web/etc/django.py /etc/gunicorn.d/django.py
 sudo /etc/init.d/gunicorn restart
